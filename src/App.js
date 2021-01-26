35b9ffcba18018213edc53e66b82089f4410d4ad
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-01-26 18:52:09 +0530

    added & updated Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
@@ -10,7 +11,11 @@ function App() {
       {/*//? Header */}
       <Header />
       {/*//Todo app body  */}
-      {/*//Todo sidebar */}
+      <div className="app__body">
+        {/*//Todo sidebar */}
+        <Sidebar />
+      </div>
+
       {/*//Todo Feed */}
       {/*//Todo widgets */}
     </div>
---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
+import PeopleIcon from '@material-ui/icons/People'
+import StorefrontIcon from '@material-ui/icons/Storefront'
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
+import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined'
+import React from 'react'
+import './Sidebar.css'
+import SidebarRow from './SidebarRow'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+
+            <SidebarRow src="AR" title="Abhishek Gowda" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yB/r/squBo4GNUfh.png" title="COVID-19 Information Center" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/-XF4FQcre_i.png" title="Friends" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/mk4dH3FK0jT.png" title="Groups" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/9BDqQflVfXI.png" title="Marketplace" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yG/r/A1HlI2LVo58.png" title="Watch" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yv/r/QAyfjudAqqG.png" title="Events" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yf/r/GA7Y4WRJMp8.png" title="Memories" />
+            <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yr/r/2uPlV4oORjU.png" title="Saved" />
+            <SidebarRow Icon={ExpandMoreOutlined} title="See More" />
+        </div>
+    )
+}
+
+export default Sidebar
