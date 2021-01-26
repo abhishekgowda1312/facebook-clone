0201fc7da1fe7d634b80ccd869525af82f3aa7ad
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-01-26 18:52:54 +0530

    updated sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,3 @@
-import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
-import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
-import PeopleIcon from '@material-ui/icons/People'
-import StorefrontIcon from '@material-ui/icons/Storefront'
-import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
 import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined'
 import React from 'react'
 import './Sidebar.css'
