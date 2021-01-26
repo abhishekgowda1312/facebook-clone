23d93d9f221c8eb5185364dd9a3324fe64fa0a89
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-01-26 19:14:09 +0530

    missed styles in sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
 
-            <SidebarRow src="AR" title="Abhishek Gowda" />
+            <SidebarRow src="https://scontent.fblr18-1.fna.fbcdn.net/v/t1.0-1/cp0/p86x86/13645123_1408458539180900_7546334927620120082_n.jpg?_nc_cat=109&ccb=2&_nc_sid=dbb9e7&_nc_ohc=xUxZOOSyM8oAX8E7zT9&_nc_ht=scontent.fblr18-1.fna&tp=27&oh=d63f106451440cbc3e1c01e99bab277a&oe=60349715" title="Abhishek Gowda" />
             <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yB/r/squBo4GNUfh.png" title="COVID-19 Information Center" />
             <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yx/r/-XF4FQcre_i.png" title="Friends" />
             <SidebarRow src="https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/mk4dH3FK0jT.png" title="Groups" />
