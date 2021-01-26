673dc64c9840a2ab5de0ffa1beacb9a779f168df
Abhishek Gowda <73712257+abhishekgowda1312@users.noreply.github.com>
2021-01-26 14:58:54 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     //?BEM naming convention
     <div className="app">
-      <h1>FaceBook</h1>
 
       {/*//? Header */}
-
+      <Header />
       {/*//Todo app body  */}
       {/*//Todo sidebar */}
       {/*//Todo Feed */}
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+
+            </div>
+            <div className="header__middle">
+                <div className="header__options">
+                    <HomeIcon />
+                </div>
+            </div>
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
